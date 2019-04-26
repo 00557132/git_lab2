402fa07baccfb420abcc964b7b4d2d92d179ca4e
林浩洋 <abluesweet@gmail.com>
2019-04-26 13:58:18 +0800

    travis_set

--- Calculator.py
+++ Calculator.py
@@ -1,6 +1,7 @@
 #master
 
 #示範Git用的程式碼！
+import unittest
 
 def add(a, b):
     return a + b
@@ -15,3 +16,25 @@ def divided(a, b):
     if b != 0:
         return a / b
     return "無限"
+	
+class TestCalculator(unittest.TestCase):
+	def test_int_add(self):
+		self.assertEqual(add(9, 3), 12)
+			
+	def test_int2_add(self):
+		self.assertEqual(add(9, 4), 13)
+		
+	def test_float_add(self):
+		self.assertEqual(add(4.2, 3), 7.2)
+		
+	def test_int_minus(self):
+		self.assertEqual(add(9, 3), 6)
+		
+	def test_int2_minus(self):
+		self.assertEqual(add(9, 4), 5)
+		
+	def test_float_minus(self):
+		self.assertEqual(add(4.2, 3.2), 1.0)
+		
+	def test_int_times(self):
+		self.assertEqual(add(9, 3), 27)
