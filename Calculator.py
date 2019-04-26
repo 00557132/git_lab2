d3bd61837f692535c7b5d70bedcf99803f9a7f15
林浩洋 <abluesweet@gmail.com>
2019-04-26 14:14:54 +0800

    cal

--- Calculator.py
+++ Calculator.py
@@ -17,24 +17,3 @@ def divided(a, b):
         return a / b
     return "無限"
 	
-class TestCalculator(unittest.TestCase):
-	def test_int_add(self):
-		self.assertEqual(add(9, 3), 12)
-			
-	def test_int2_add(self):
-		self.assertEqual(add(9, 4), 13)
-		
-	def test_float_add(self):
-		self.assertEqual(add(4.2, 3), 7.2)
-		
-	def test_int_minus(self):
-		self.assertEqual(add(9, 3), 6)
-		
-	def test_int2_minus(self):
-		self.assertEqual(add(9, 4), 5)
-		
-	def test_float_minus(self):
-		self.assertEqual(add(4.2, 3.2), 1.0)
-		
-	def test_int_times(self):
-		self.assertEqual(add(9, 3), 27)
